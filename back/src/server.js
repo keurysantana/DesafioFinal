897319b6e342879ffdb9fe17a2770932186ca6bb
Keury Santana <keurytsantana@gmail.com>
2021-09-12 20:58:48 +0100

    Adiciona tmdb na pagina de detalhes

--- back/src/server.js
+++ back/src/server.js
@@ -3,6 +3,7 @@ const express = require ('express')
 const app = express()
 //const dataBase = require('./database/databaseKnex')
 const bodyParser = require('body-parser')
+const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -25,5 +26,34 @@ app.get ('/listaPrincipal', async(req, res) => {
     ] 
     res.send(listaSeries)
 })
+
+app.get('/detalheSerie/:id', async(req, res) => {
+    var config = {
+        method: 'get',
+        url: 'https://api.themoviedb.org/3/tv/'+req.params.id+'?api_key=7bd965a9aa82c3b1f149fb83984aa103&language=pt-PT',
+        headers: { }
+    };
+
+    axios(config)
+    .then(function (response) {
+        let genreName=[]
+        for(let genre of response.data.genres){
+            genreName.push(genre.name)
+        }
+        const detalheSerie ={
+            urlFoto:"http://image.tmdb.org/t/p/w185/"+response.data.poster_path,
+            nome: response.data.original_name,
+            ano: response.data.first_air_date,
+            avaliacoes: response.data.vote_average,
+            classificacao: genreName.join(', '),
+            sinopse: response.data.overview
+        }
+        res.send(detalheSerie)
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+})
+
 
 app.listen(3003)
---
+++ front/js/detalhes.js
@@ -0,0 +1,20 @@
+var parametros= location.search.substring(1);
+var idSerie = parametros.split('=')[1];
+
+async function getDetalhe(id) {
+    try {
+        const response = await fetch('http://localhost:3003/detalheSerie/'+id)
+        const data = await response.json()
+        console.log(data)
+        $("#imagem").prepend('<img src="'+data.urlFoto+'">');
+        $("#nome").prepend(data.nome)
+        $("#ano").prepend(data.ano)
+        $("#avaliacoes").prepend(data.avaliacoes)
+        $("#classificacao").prepend(data.classificacao)
+        $("#sinopse").prepend(data.sinopse)
+    } catch (error) {
+        console.log('falha' + error)
+    }
+}
+
+getDetalhe(idSerie)
